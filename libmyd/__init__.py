353685638c2a75dbe1fffba122f5a78cdc487bcb
Marco Antonio Islas Cruz <markuz@islascruz.org>
2011-07-26 16:48:52 -0500

    updating license in files

---
+++ libmyd/__init__.py
@@ -0,0 +1,16 @@
+# This file is part of the markuz_youtube_downlaoder project
+#
+# Copyright (c) 2011 Marco Antonio Islas Cruz
+#
+# markuz_youtube_downlaoder is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License.
+#
+# markuz_youtube_downlaoder is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
